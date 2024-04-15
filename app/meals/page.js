e274d6c5f7d4154665f43f691d02cad5a6a19000
CosminDigitablAssets <cosmin@digitalassets.ro>
2024-04-15 15:29:10 +0300

    Next JS App Router

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,18 +1,41 @@
+import MealsGrid from '@/components/meals/meals-grid'
+import classes from './page.module.css'
 import Link from 'next/link'
+import { getMeals } from '@/lib/meals'
+import { Suspense } from 'react'
 
-export default function Meals() {
+export const metadata = {
+  title: 'All Meals',
+  description: 'Browse the delicious meals shared by our vibrant community.',
+}
+
+async function Meals() {
+  const meals = await getMeals()
+  return <MealsGrid meals={meals} />
+}
+
+export default function MealsPage() {
   return (
     <>
-      <p>
-        <Link href="/meals/share">SHARE</Link>
-      </p>
-      <p>
-        <Link href="/meals/meal-1">MEAL1</Link>
-      </p>
-      <p>
-        <Link href="/meals/meal-2">MEAL2</Link>
-      </p>
-      <h1>MEALS PAGEE</h1>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{''}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals..</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
     </>
   )
 }
